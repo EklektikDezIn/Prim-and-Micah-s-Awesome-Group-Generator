4b35504193f793e00462c816309cca4a437ba678
EklektikDezin <Eklektik.DezIn@gmail.com>
2019-04-07 14:08:22 -0500

    first commit files

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Groups
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 8);
+            this.button1.MinimumSize = new System.Drawing.Size(60, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(128, 5);
+            this.label1.MinimumSize = new System.Drawing.Size(50, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(502, 244);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown1.Location = new System.Drawing.Point(378, 8);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.MinimumSize = new System.Drawing.Size(5, 0);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 4);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(496, 157);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button2, 2);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(128, 206);
+            this.button2.MinimumSize = new System.Drawing.Size(50, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(328, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Groups:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Names";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\Mishko\\Desktop";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 244);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(518, 282);
+            this.Name = "Form1";
+            this.Text = "Prim and Micah\'s Awesome Group Generator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Groups
+{
+    public partial class Form1 : Form
+    {
+        string path = "null";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            path = openFileDialog1.FileName;
+            label1.Text = path;
+            UpdateFields(path);
+        }
+        private void UpdateFields(String path)
+        {
+            int groupNum = (int)numericUpDown1.Value;
+            List<String> Bowl = Read(path);
+            int eqDivide = Bowl.Count + (groupNum - (Bowl.Count % groupNum));
+            String[,] Micah = new String[groupNum, eqDivide / groupNum];
+            Micah = setEmpty(Micah);
+            int stop = Math.Abs(Bowl.Count - eqDivide);
+            for (int i = 0; i < stop; i++)
+            {
+                Bowl.Add(" ");
+                Micah[i % groupNum, (eqDivide / groupNum) - 1] = " ";
+            }
+            int[] Prim = new int[groupNum];
+            for (int i = 0; i < Prim.Length; i++)
+            {
+                Prim[i] = 0;
+            }
+            Random rand = new Random();
+            foreach (String Fish in Bowl)
+            {
+                if (!Fish.Equals(" "))
+                {
+                    int sect;
+                    do
+                    {
+                        sect = (int)(rand.NextDouble() * 100) % groupNum;
+                    } while ((eqDivide / groupNum - Prim[sect] == 0) || ((eqDivide / groupNum - Prim[sect] == 1) && (Micah[sect, (eqDivide / groupNum) - 1].Equals(" "))));
+
+                    Micah[sect, Prim[sect]] = Fish;
+                    Prim[sect]++;
+                }
+
+            }
+            String groups = "";
+            for (int Nathan = 0; Nathan < groupNum; Nathan++)
+            {
+                groups += "Group " + (Nathan + 1) + "\r\n----------\r\n";
+                for (int Chris = 0; Chris < (Prim[Nathan]); Chris++)
+                {
+                    groups += Micah[Nathan, Chris] + "\r\n";
+                }
+                groups += "\r\n \r\n";
+
+            }
+            textBox1.Text = groups;
+
+        }
+        public static String[,] setEmpty(String[,] inpt)
+        {
+            for (int i = 0; i < inpt.GetLength(0); i++)
+            {
+                for (int j = 0; j < inpt.GetLength(1); j++)
+                {
+                    inpt[i, j] = "";
+                }
+            }
+            return inpt;
+        }
+        public static List<String> Read(String file)
+        {//READS A FILE
+            List<String> Paragraph = new List<String>();
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(file);
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //Add the line to the list
+                    Paragraph.Add(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //Close the file
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception at readFile: " + e.Message);
+            }
+            return Paragraph;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (path.Equals("null"))
+            {
+                textBox1.Text = textBox1.Text + "Please select a file.\r\n";
+            }
+            else
+            {
+                UpdateFields(path);
+            }
+        }
+    }
+}
